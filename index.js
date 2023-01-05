1faf38b20c9bf713b6837f5ca6618d0f8eb7233c
Roman <sayga76239@gmail.com>
2023-01-05 14:43:57 +0100

    в

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
     const container =  document.querySelector('.container');
 
     const hoverHandler = (e) => {
-        const { target: button } = e;//определяем кнопку
+        const { target: button } = e;//берем из события ивент его target и называем его button. 
         button.style.top = `${Math.floor(Math.random() * (window.innerHeight - button.clientHeight))}px`;//назначаем рондомное положение кнопки
         button.style.left = `${Math.floor(Math.random() * (window.innerWidth - button.clientWidth))}px`;
         console.log(button.clientHeight);//получаем размер кнопки через ее свойство
