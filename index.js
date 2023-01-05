fd9ce90d09abf2472081048c48ef26dacb0201f1
Roman <sayga76239@gmail.com>
2023-01-05 12:49:06 +0100

    ss

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
     const hoverHandler = (e) => {
         const { target: button } = e;//
-        button.style.top = `${Math.floor(Math.random() * 1000)}px`;
-        button.style.left = `${Math.floor(Math.random() * 1000)}px`;
+        button.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
+        button.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
     };
     container.addEventListener("mouseover", hoverHandler);
